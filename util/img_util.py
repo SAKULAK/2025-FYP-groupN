9591bd05d1224273f164257815ccba90c561b1c4
Lukáš Trstenský <skyskjepan@gmail.com>
2025-02-12 10:25:06 +0100

    Chenged the ImageDataLoader class in img_util.py to the one provided. Made loop for hair removal in main.py.

--- util/img_util.py
+++ util/img_util.py
@@ -1,7 +1,7 @@
-import random
 import os
+import random
+
 import cv2
-import skimage
 
 
 def readImageFile(file_path):
@@ -33,75 +33,37 @@ def saveImageFile(img_rgb, file_path):
         return False
 
 
-class ImageDataLoader():
+class ImageDataLoader:
+    def __init__(self, directory, shuffle=False, transform=None):
+        self.directory = directory
+        self.shuffle = shuffle
+        self.transform = transform
 
-    def __init__(self, directory: str, shuffle: bool=False, transform: bool=False):
-        """"Load directory of images."""
-        self.__iter_num__ = 0
-        self.directory: str = directory
-        self.shuffle: bool = shuffle
-        self.transform: bool = transform
-        self.fileList: list = sorted(
+        # get a sorted list of all image files in the directory
+        self.file_list = sorted(
             [os.path.join(directory, f) for f in os.listdir(directory) if
              f.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff'))]
         )
 
-        if not self.fileList:
-            raise ValueError("Image files not found in the directory.")
-        
+        if not self.file_list:
+            raise ValueError("No image files found in the directory.")
+
+        # shuffle file list if required
         if self.shuffle:
-            random.shuffle(self.fileList)
+            random.shuffle(self.file_list)
 
-        self.num_files: int = len((self.fileList))
-        
-        self.image_data: list = list()
-
-    def FetchData(self, denoising: bool=True, histogram_equalization: bool=True):
-        """Loads image data into a list with each entry being [imgage_bgr_data, image_rgb_data, image_greyscale_data]"""
-        self.image_data = [[raw_data := cv2.imread(img_path), cv2.cvtColor(raw_data, cv2.COLOR_BGR2RGB), cv2.cvtColor(raw_data, cv2.COLOR_BGR2GRAY)] for img_path in self.fileList]
-        if denoising:
-            for image in self.image_data:
-                image[0] = cv2.fastNlMeansDenoisingColored(image[0],None,10,10,7,21)
-                image[1] = cv2.fastNlMeansDenoisingColored(image[1],None,10,10,7,21)
-                image[2] = cv2.fastNlMeansDenoising(image[2],None,10,7,21)
-            pass
-        if histogram_equalization:
-            for image in self.image_data:
-                image[2] = skimage.exposure.equalize_hist(image[2])
-            pass
-        return self.image_data
-    
-    def DisplayImage(self, index: int, color: str = "bgr"):
-        """Displays an image from ImageDataLoader.image_data. Color must be "bgr", "rgb, or "gray".
-        If data has not been fetched, runs ImageDataLoader.FetchData
-        """
-
-        if not self.image_data:
-            self.FetchData()
-
-        def showImg(image):
-            cv2.imshow("Image", image)
-            cv2.waitKey(0)
-        
-        translation_dict = {"bgr": 0, "rgb": 1, "gray": 2}
-        try:
-            image = self.image_data[index][translation_dict[color]]
-
-        except KeyError as e:
-            raise KeyError("Color data not found. Color variable must be 'bgr', 'rgb', or 'gray'.\n", e)
-        
-        showImg(image)
+        # get the total number of files
+        self.num_sample = len(self.file_list)
 
     def __len__(self):
-        return self.num_files
-    
+        return self.num_sample
+
     def __iter__(self):
-        self.__iter_num__ = 0
-        return self
-    
-    def __next__(self):
-        if self.__iter_num__+1 > self.num_files:
-            raise StopIteration
-        else:
-            self.__iter_num__ += 1
-            return self.image_data[self.__iter_num__-1]
+        for file_path in self.file_list:
+            img_rgb, img_gray = readImageFile(file_path)
+
+            if self.transform:
+                img_rgb = self.transform(img_rgb)
+                img_gray = self.transform(img_gray)
+
+            yield img_rgb, img_gray
